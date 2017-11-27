934d3bd53e0bc5447bae6c572081faa030908382
stkyle <stkyle@users.noreply.github.com>
2017-11-27 12:23:04 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,250 +1,184 @@
-/**
- * Crypto Implementation
- *
- * Pre-requisite: RSA Public Key File
- *     - Generating RSA Key Pair:
- *         1. Create Private Key: openssl genrsa -out privkey 2048
- *         2. Use Private Key to create Public Key: openssl rsa -in privkey.pem -pubout pubkey.pem
- */
-
-#include <stdio.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 #include <string.h>
 
-#include <openssl/rsa.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
-#include <openssl/rand.h>
+#include "slurpfile.h"
+#include "crypto.h"
 
-
-void handle_error(char * msg);
-void init_libcrypto(void);
-
-char pub_key_text[] = "-----BEGIN PUBLIC KEY-----\n"\
-        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwT/yK9RJ+gxYhXkwWkSc\n"\
-        "Yo4e0g2h/ct9ETcL0YQJDT8v49bmtqZVcg7pNeT2TV3NqrJfYdBAhEkbhY0r+Zg2\n"\
-        "paFrkz/e+AtzHf6Farstwt3r6SZ8HVNgq9UrT5U8VeqRMqQclb2wid3f3lpKT312\n"\
-        "kPO+Mmee4JOwXpC+8Zc3KgZ43g653Q3ufj4VOHMywQTY5HIiAVl+diPuLwltX8gd\n"\
-        "kpRWv+rEbz5zpCMKtNjbgGL4oPJq/oHCdRDOlCpPMqLMnEIDwTmx0x8c9+gQHVMd\n"\
-        "sSPKfL+XsrQmLqw/a9GzkDYr00e/8LbEeERHwRmwtkziBJ4Dammy6VTC8H5KEsXS\n"\
-        "3QIDAQAB\n"\
-        "-----END PUBLIC KEY-----";
-
-RSA * load_pubkey(unsigned char * key)
-{
-    /* Read PEM Encoded Public Key from memory into an RSA Structure */
-    RSA *rsa= NULL;
-    BIO *keybio ;
-    keybio = BIO_new_mem_buf(key, -1);
-    rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa,NULL, NULL);
-    return rsa;
+void handleErrors(){
+    printf("ERROR\n");
 }
 
-long slurpfile(char const* path, char **buf, int add_nul)
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
 {
-    FILE  *fp;
-    size_t fsz;
-    long   off_end;
-    int    rc;
+    EVP_CIPHER_CTX *ctx;
 
-    /* Open the file */
-    fp = fopen(path, "rb");
-    if( NULL == fp ) {
-        return -1L;
-    }
+    int len;
 
-    /* Seek to the end of the file */
-    rc = fseek(fp, 0L, SEEK_END);
-    if( 0 != rc ) {
-        return -1L;
-    }
+    int ciphertext_len;
 
-    /* Byte offset to the end of the file (size) */
-    if( 0 > (off_end = ftell(fp)) ) {
-        return -1L;
-    }
-    fsz = (size_t)off_end;
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-    /* Allocate a buffer to hold the whole file */
-    *buf = malloc( fsz+(int)add_nul );
-    if( NULL == *buf ) {
-        return -1L;
-    }
+    /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
 
-    /* Rewind file pointer to start of file */
-    rewind(fp);
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        handleErrors();
+    ciphertext_len = len;
 
-    /* Slurp file into buffer */
-    if( fsz != fread(*buf, 1, fsz, fp) ) {
-        free(*buf);
-        return -1L;
-    }
+    /* Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+    ciphertext_len += len;
 
-    /* Close the file */
-    if( EOF == fclose(fp) ) {
-        free(*buf);
-        return -1L;
-    }
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
 
-    if( add_nul ) {
-        /* Make sure the buffer is NUL-terminated, just in case */
-        buf[fsz] = '\0';
-    }
-
-    /* Return the file size */
-    return (long)fsz;
+    return ciphertext_len;
 }
 
 
-RSA * getPubKeyFromText(unsigned char * key)
-{
-    RSA *rsa= NULL;
-    BIO *keybio ;
-    keybio = BIO_new_mem_buf(key, -1);
-    if (keybio==NULL)
-    {
-        printf( "Failed to create key BIO");
-        return 0;
-    }
-    rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa,NULL, NULL);
-    if(rsa == NULL)
-    {
-        printf( "Failed to create RSA");
-    }
 
-    return rsa;
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+    /* Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
+
+    /* Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
 }
 
-int public_encrypt(unsigned char * data, int data_len, RSA * pubkey, unsigned char *encrypted)
+
+
+
+int main (void)
 {
-    int result = RSA_public_encrypt(data_len,data,encrypted,pubkey,RSA_PKCS1_PADDING);
-    return result;
-}
+  /* Set up the key and iv. Do I need to say to not hard code these in a
+   * real application? :-)
+   */
+    int npubk = 1;
+    unsigned char **ek = (unsigned char **)malloc(sizeof(unsigned char *) * npubk);
+    int *ekl = (int *)malloc(sizeof(int) * npubk);
+    unsigned char iv[16];
+
+    char *buffer = NULL;
+    /* Message to be encrypted */
+    unsigned char *plaintext =
+                (unsigned char *)"The quick brown fox jumps over the lazy dog";
 
 
-int main(){
-    printf("Crypto Implementation\n");
+    unsigned char ciphertext[1024];
 
-    // declarations
-    const char * pub_keyfile_name = "../pubkey.pem";
-    const EVP_CIPHER* pCipher = NULL;
-    FILE*     pFile    = NULL;
-    EVP_PKEY* pPubKey  = NULL;
-    RSA* pRsaKey = NULL;
+    /* Buffer for the decrypted text */
+    unsigned char decryptedtext[1024];
 
+    /* Initialise the library */
+    ERR_load_crypto_strings();
+    OpenSSL_add_all_algorithms();
+    OPENSSL_config(NULL);
 
-    // init crypto lib
-    init_libcrypto();
-
-    // load public key from file
-
-    //pFile = fopen( pub_keyfile_name, "r");
-    char * buffer;
-    long pemSize = slurpfile(pub_keyfile_name, &buffer, 1);
+    slurpfile("../pubkey.pem", &buffer, 1);
     printf("%s\n", buffer);
-    printf("pemSize :%d\n", (int) pemSize);
-    printf("strlen :%d\n", (int) strlen(buffer));
-    pRsaKey = getPubKeyFromText(buffer);
+    RSA *rsa = getPubKeyFromPemStr(buffer);
+
+    EVP_PKEY *pubkey = EVP_PKEY_new();
+    EVP_PKEY_set1_RSA(pubkey, rsa);
+    EVP_PKEY **pubk = (EVP_PKEY **)malloc(sizeof(EVP_PKEY *) * npubk);
+    pubk[0] = pubkey;
+    ek[0] = (unsigned char *)malloc(EVP_PKEY_size(pubk[0]));
+    printf("PKEY Created\n");
+    //ciphertext_len = envelope_seal(&pubkey, plaintext,(int) strlen(plaintext), &ek, &encrypted_key_len, iv, ciphertext );
+    //printf("cypher length: %d\n", ciphertext_len);
+    //EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
 
 
-    char * number_str = BN_bn2hex(pRsaKey->e);
+    //EVP_SealInit(ctx, EVP_aes_256_cbc(), ek, ekl, iv, pubk, npubk);
+    //int retcode = EVP_SealInit(ctx,EVP_aes_256_cbc(), ek, ekl, iv, pubk, 1);
+    int ciphertext_len = envelope_seal(pubk,plaintext, (int) strlen(plaintext),ek, ekl, iv, ciphertext);
+    printf("\nenvelope_seal (ciphertext_len): %d", ciphertext_len);
+    printf("\nenvelope_seal (iv): %s", iv); // aes initial val
+    printf("\nenvelope_seal (sizeof(iv)): %d", (int) sizeof(iv));
+    printf("\nenvelope_seal (ek0): %s", ek[0]); // should be size 16 -encrypted key
+    printf("\nenvelope_seal (ekl0): %d", ekl[0]); // should be 256 - encrypted key length
+    printf("\nenvelope_seal (ciphertext): %s\n", ciphertext);
+    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+    
+    
+    
 
-    printf("exponent: %s\n", number_str);
-    free(buffer); // free heap space
 
-    unsigned char plainText[2048/8] = "this is some dummy text"; //key length : 2048
-    unsigned char  encrypted[4098]={};
-    unsigned char decrypted[4098]={};
 
-    int enc_length = public_encrypt(plainText, (int) strlen(plainText), pRsaKey, encrypted);
-    printf("length: %d\n", enc_length);
-    printf("encrypted data: %s", (const char *) encrypted, enc_length);
 
-    //sprintf("enc  length: %d", enc_length);
-    //RSA *PEM_read_RSA_PUBKEY(FILE *fp, RSA **x,
-    //                         pem_password_cb *cb, void *u);
 
-    //pub_key = PEM_read_RSA_PUBKEY(pub_keyfile, NULL, NULL, NULL);
-    //pub_key = load_pubkey(pub_key_text);
-    //printf("modulus: %d\n", (int) pub_key->n);
-    //printf("exp: %d\n", (int) pub_key->e);
-//    if(RSA_check_key(pub_keyfile_rsa)) {
-//        printf("RSA key is valid.\n");
-//    }
-//    else {
-//        printf("Error validating RSA key.\n");
-//    }
-    //unsigned char * modulus;
-    //unsigned char * exp;
-    //BN_bn2bin(pub_keyfile_rsa->n, (unsigned char *)modulus);
-    //BN_bn2bin(pub_keyfile_rsa->e, (unsigned char *)exp);
-    //printf("\n%s\n%s\n", exp, modulus);
+    //EVP_CIPHER_CTX_free(ctx);
+    EVP_PKEY_free(pubkey);
+    free(buffer);
+    free(pubk);
+    exit(0);
+//  /* Encrypt the plaintext */
+//  ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+//                            ciphertext);
+//
+//  /* Do something useful with the ciphertext here */
+//  printf("Ciphertext is:\n");
+//  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+//
+//  /* Decrypt the ciphertext */
+//  decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+//    decryptedtext);
+//
+//  /* Add a NULL terminator. We are expecting printable text */
+//  decryptedtext[decryptedtext_len] = '\0';
+//
+//  /* Show the decrypted text */
+//  printf("Decrypted text is:\n");
+//  printf("%s\n", decryptedtext);
 
-    // cleanup
-    //fclose(pub_keyfile);
-    RSA_free(pRsaKey);
+  /* Clean up */
+  EVP_cleanup();
+  ERR_free_strings();
+
+  return 0;
 }
-
-
-void init_libcrypto(void)
-{
-    if(1)
-    {
-        //SSL_load_error_strings();
-        //OpenSSL_add_all_algorithms();
-        RAND_load_file("/dev/urandom", 1024);
-    }
-    else
-        exit(EXIT_FAILURE);
-}
-
-void cleanup_openssl(void)
-{
-    CRYPTO_cleanup_all_ex_data();
-    ERR_free_strings();
-    ERR_remove_thread_state(0);
-    EVP_cleanup();
-}
-
-void handle_openssl_error(void)
-{
-    ERR_print_errors_fp(stderr);
-}
-
-void handle_error(char * msg)
-{
-    perror(msg);
-}
-
-
-
-//CMakeLists.txt
-set(CMAKE_VERBOSE_MAKEFILE OFF)
-cmake_minimum_required(VERSION 3.8)
-project(mytestproject)
-
-set(CMAKE_C_STANDARD 99)
-SET(CMAKE_SYSTEM_NAME Linux)
-
-
-
-
-MESSAGE( STATUS "CMAKE_BINARY_DIR:         " ${CMAKE_BINARY_DIR} )
-set(SOURCE_FILES oldmain.c config.h certpubkey.c encrypt.c decrypt.c crypt.c main.c)
-add_executable(mytestproject ${SOURCE_FILES})
-
-
-
-
-# OpenSSL/libcrypto (find, include, link)
-find_package(OpenSSL REQUIRED)
-message(STATUS "OPENSSL_VERSION: ${OPENSSL_VERSION}")
-message(STATUS "OPENSSL_CRYPTO_LIBRARIES: ${OPENSSL_CRYPTO_LIBRARIES}")
-include_directories(${OPENSSL_INCLUDE_DIR})
-target_link_libraries(${PROJECT_NAME} ${OPENSSL_CRYPTO_LIBRARIES})
-
-
-#get_cmake_property(_variableNames VARIABLES)
-#foreach (_variableName ${_variableNames})
-#    message(STATUS "${_variableName}=${${_variableName}}")
-#endforeach()
